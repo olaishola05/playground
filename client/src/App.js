e03f68cf66e919433d03b4d3951286c90681eab4
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-09-07 20:55:53 +0100

    routes in the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
-// import Home from "./components/Home";
+import Home from "./components/Home";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
-// import Profile from "./components/Profile";
+import Profile from "./components/Profile";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      {/* <Home/> */}
-      {/* <Profile /> */}
-      {/* <Login /> */}
-      <Register />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
